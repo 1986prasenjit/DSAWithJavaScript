94918f4cfa521cfaae9d3cfca2a185ec1f1ebe79
1986prasenjit <prasenjitbasu44@gmail.com>
2025-07-04 01:37:56 +0530

    added a new problem on Array moveAllTheZeros

---
+++ BasicProblemsOnArray/moveAllTheZeros.js
@@ -0,0 +1,34 @@
+/*
+Given an integer array nums, move all 0’s to the end of it while maintaining the relative order of the non-zero elements.
+
+Note: You must do this in-place without making a copy of the array.
+
+Examples
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+Input: nums = [0]
+Output: [0]
+Constraints:
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 – 1
+*/
+
+function moveTheZeros(arr) {
+    let x = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 0) {
+            arr[x] = arr[i];
+            x = x + 1
+        }
+    }
+    for (let i = x; i < arr.length; i++) {
+        arr[i] = 0
+    }
+    return arr;
+}
+
+let result = moveTheZeros([0, 1, 0, 3, 12]);
+
+console.log(result);
