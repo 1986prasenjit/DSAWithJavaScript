22ecac60b85f2df26f245e1639f6076b1838319e
1986prasenjit <prasenjitbasu44@gmail.com>
2025-09-28 15:48:07 +0530

    added a new function to count vowels in a string

---
+++ BasicProblemwithJS/countVowels.js
@@ -0,0 +1,16 @@
+const countval = (str) => {
+  let count = 0;
+  for (let char of str) {
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+};
+console.log(countval("amit"));
